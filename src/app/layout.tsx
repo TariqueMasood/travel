0d5d3b336c403f0515ae6a62ab83ff5460f9990a
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-05-24 15:47:07 +0530

    ECE-402: grafana removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
-import { getWebInstrumentations, initializeFaro } from "@grafana/faro-react";
-import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -12,26 +10,6 @@ export const metadata: Metadata = {
   title: "Travel & Tour App",
   description: "Travel & Tour App",
 };
-
-initializeFaro({
-  // Mandatory, the URL of the Grafana collector
-  url: "https://akramaltaf786.grafana.net/a/grafana-kowalski-app/apps",
-
-  // Mandatory, the identification label of your application
-  app: {
-    name: "Traveller",
-  },
-
-  instrumentations: [
-    // Load the default Web instrumentations
-    ...getWebInstrumentations(),
-
-    // Tracing Instrumentation is needed if you want to use the React Profiler
-    new TracingInstrumentation(),
-
-    // ...
-  ],
-});
 
 export default function RootLayout({
   children,
