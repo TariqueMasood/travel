bf227f694cb54df678bd69264a2ecc8e106fce2e
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-05-24 14:34:31 +0530

    ECE-402: Granfana url added

--- src/utils/grafana.ts
+++ src/utils/grafana.ts
@@ -1,15 +1,13 @@
-import { matchRoutes } from 'react-router-dom';
-
 import { getWebInstrumentations, initializeFaro } from '@grafana/faro-react';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 initializeFaro({
   // Mandatory, the URL of the Grafana collector
-  url: 'my/collector/url',
+  url: 'https://akramaltaf786.grafana.net/a/grafana-kowalski-app/apps',
 
   // Mandatory, the identification label of your application
   app: {
-    name: 'my-react-app',
+    name: 'Travel',
   },
 
   instrumentations: [
